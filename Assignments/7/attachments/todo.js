66ec661a1ae21a76532a1912b07306b1ee777046
Oisov <Oistein.Sovik.92@gmail.com>
2017-11-28 21:08:20 +0100

    Fixed 7 so that tasks start at top. Added shorter code

--- Assignments/7/attachments/todo.js
+++ Assignments/7/attachments/todo.js
@@ -13,25 +13,29 @@ function addTask() {
     checkbox.type = 'checkbox';
     checkbox.setAttribute('onchange', 'numberFunction()');
 
+    // Create a label for the checkbox, this is the task to complete
     const label = document.createElement('label');
     const t = document.createTextNode(input.value);
     label.setAttribute('class', 'completed')
     label.appendChild(t);
 
+    // Append the label and the checkbox to an div element called todo
     const todo = document.createElement('div');
     todo.appendChild(checkbox);
     todo.appendChild(label);
 
-    // PART 3 - Stores tasks as objects
+    // PART 3 - Stores tasks as objects, use timestamp as key as these are unique
     const tasks = {};
     tasks[timestamp()] = todo;
     console.log(tasks); // Prints tasks to console for debugging
 
-    todolist.appendChild(todo);
+    //Make sure to insert the new todo at the top of the list
+    todolist.insertBefore(todo, todolist.childNodes[0]);
     input.value = ''; //Clear the input field afterwards.
     numberFunction() // Updates the total number of tasks
 };
 
+// Creates a timestamp to be used as the keys for the todlist
 function timestamp() {
     const oldDate = new Date(1970, 1, 1, 0, 0, 0);
     const nowDate = new Date();
@@ -39,15 +43,29 @@ function timestamp() {
     return timeSince1970
 }
 
-// Returns number of completed tasks
+// Returns number of completed tasks. Very slick
+// Use const since functions should be used with inputs
 const checkIfChecked = () => {
+    var count = 0;
     const checkbox = document.querySelectorAll("input[type=checkbox]");
-    let number = 0;
-    for (let i = 0; i < checkbox.length; i++) {
-        checkbox[i].checked ? number++ : null;
-    }
-    return number;
-};
+    // Inline for-loop. v.checked is a boolean expression: True or False.
+    // ? is a if sentence (ternary expression) If v.checked increase count else null
+    checkbox.forEach(v => v.checked ? count++ : null);
+    return count
+}
+
+// A more sane way to return number of completed tasks. Uncomment to use
+
+// function checkIfChecked () {
+//     const checkbox = document.querySelectorAll("input[type=checkbox]");
+//     let number = 0;
+//     for (let i = 0; i < checkbox.length; i++) {
+//         if (checkbox[i].checked) {
+//             number++;
+//         }
+//     }
+//     return number;
+// };
 
 // PART 5 - Everytime a task is completed, unchecked or added this function
 // updates the number of completed tasks. 
